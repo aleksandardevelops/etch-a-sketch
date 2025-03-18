b743f117c98fc30868fc4a9f8809a229ec56a79f
aleksandardevelops <119680237+aleksandardevelops@users.noreply.github.com>
2025-03-18 18:53:28 +0100

    Add two loops to redesign the 16x16 flex grid and implement on hover tile syling

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const flexContainer = document.querySelector('.flex-container');
 
-for (i = 0; i < 256; i++) {
-  const tile = document.createElement('div');
-  flexContainer.appendChild(tile);
-  tile.style.backgroundColor = 'blue';
-  tile.style.width = '1em';
-  tile.style.height = '1em';
+for (i = 0; i < 16; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  flexContainer.appendChild(row);
+
+  for (k = 0; k < 16; k++) {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    row.appendChild(tile);
+
+    tile.style.width = '2em';
+    tile.style.height = '2em';
+  }
 }
