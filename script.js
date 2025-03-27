6f90fb20cc8d56bf5198191988c559f9d7e42acd
aleksandardevelops <119680237+aleksandardevelops@users.noreply.github.com>
2025-03-27 01:15:42 +0100

    Add dynamic size for tiles

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const flexContainer = document.querySelector('.flex-container');
 
 let slider = document.getElementById('myRange');
 let output = document.querySelector('.density-output');
+let containerSize = flexContainer.clientWidth;
 
 slider.onchange = function () {
   output.innerHTML = this.value;
@@ -10,6 +11,7 @@ slider.onchange = function () {
 
 function createGrid(size) {
   flexContainer.innerHTML = '';
+  let tileSize = containerSize / size;
   for (let i = 0; i < size; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
@@ -20,14 +22,18 @@ function createGrid(size) {
       tile.classList.add('tile-class');
       row.appendChild(tile);
       tile.style.backgroundColor = 'aqua';
-      if (size < 20) {
-        tile.style.width = '3em';
-        tile.style.height = '3em';
-      } else {
-        tile.style.width = '0.5em';
-        tile.style.height = '0.5em';
-      }
-      tile.style.backgroundColor = 'aqua';
+      tile.style.width = tileSize + 'px';
+      tile.style.height = tileSize + 'px';
+
+      //   if (size < 20) {
+      //     tile.style.width = '3em';
+      //     tile.style.height = '3em';
+      //   } else {
+      //     tile.style.width = '0.5em';
+      //     tile.style.height = '0.5em';
+      //   }
+      //   tile.style.backgroundColor = 'aqua';
+      // }
     }
   }
   let allTiles = document.querySelectorAll('.tile-class');
